0cece0ed7c82fd20c52160a568851abad8172a44
Marcelo <marcelosen4@gmail.com>
2020-12-18 18:33:37 -0300

    agregado mongoose para conexion a db

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
+require('dotenv').config(); // para crear variables de entorno
+
 const app = require('./server');
+require('./database');
 
 app.listen(app.get('port'), () => {
     console.log('Server on port', app.get('port'));
