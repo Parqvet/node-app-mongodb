2d5e73c2bda8cd4d336b499408cae00b01b59d08
Marcelo <marcelosen4@gmail.com>
2020-12-20 11:27:01 -0300

    agregado view engine handlebars

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 const path = require('path');
 
 // initializations
@@ -6,16 +7,25 @@ const app = express();
 
 // settings
 app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'))
+app.set('views', path.join(__dirname, 'views'));
+app.engine('hbs', exphbs({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
 
 // middlewares
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({
+    extended: false
+}));
 
 // global variables
 
 // routes
 app.get('/', (req, res) => {
-    res.send('Hello world');
+    res.render('index');
 })
 
 // static files
