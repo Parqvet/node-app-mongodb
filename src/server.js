c4e5410b1ba27c05844cdb2b63f739bc8fa5d344
Marcelo <marcelosen4@gmail.com>
2021-01-01 10:34:03 -0300

    agregado users routes y vistas para el login y el register de usuarios

--- src/server.js
+++ src/server.js
@@ -47,6 +47,7 @@ app.use((req, res, next) => {
 // routes
 app.use(require('./routes/index.routes'));
 app.use(require('./routes/notes.routes'));
+app.use(require('./routes/users.routes'));
 
 // static files
 app.use(express.static(path.join(__dirname, 'public')));
